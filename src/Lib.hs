f5622875f6852b3e451dc60bc54a8e6316dfe6ee
Hercules Merscher <hlmerscher@gmail.com>
2015-12-10 22:35:51 -0200

    day 1 part 1

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,6 +2,14 @@ module Lib
     ( answers
     ) where
 
+day1 :: String -> Int
+day1 content = sum $ map toNum content
+               where
+                  toNum '(' = 1
+                  toNum ')' = -1
+                  toNum _   = 0
+
 answers :: IO ()
 answers = do
-    putStrLn $ "day 1 = " ++ "1"
+    content <- readFile "day1.txt"
+    putStrLn $ "day 1 = " ++ show (day1 content)
