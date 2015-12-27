0b5ae251b3e3937610c9bde8ce0cb9a8d4eae3a9
Hercules Merscher <hlmerscher@gmail.com>
2015-12-27 20:02:02 -0200

    day 8 part 2

--- src/Day8.hs
+++ src/Day8.hs
@@ -4,17 +4,31 @@ module Day8
 
 countChars :: String -> Int
 countChars ('\\':'\\':ws)    = 1 + countChars ws
-countChars ('\\':'\"':ws)    = 1 + countChars ws
+countChars ('\\':'"':ws)     = 1 + countChars ws
 countChars ('\\':'x':_:_:ws) = 1 + countChars ws
 countChars (_:ws)            = 1 + countChars ws
 countChars []                = -2
 
-total :: String -> Int
+countCharsEncoded :: String -> Int
+countCharsEncoded ('\\':ws)    = 2 + countCharsEncoded ws
+countCharsEncoded ('"':ws)     = 2 + countCharsEncoded ws
+countCharsEncoded (_:ws)       = 1 + countCharsEncoded ws
+countCharsEncoded []           = 2
+
+literals :: [String] -> Int
+literals words' = sum $ map length words'
+
+total, total2 :: String -> Int
+
 total strings = let words'   = words strings
-                    literals = sum $ map length words'
                     inMemory = sum $ map countChars words'
-                in literals - inMemory
+                in (literals words') - inMemory
+
+total2 strings = let words'  = words strings
+                     encoded = sum $ map countCharsEncoded words'
+                 in encoded - (literals words')
 
 answers :: String -> IO ()
 answers content = do
     putStrLn $ "day 8 part 1 = " ++ show (total content)
+    putStrLn $ "day 8 part 2 = " ++ show (total2 content)
